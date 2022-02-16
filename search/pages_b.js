6a2d2b7a886b21b93cece8de20b4f40893c2437c
vilvarajintel <vilvarajintel@users.noreply.github.com>
2022-02-16 17:43:42 +0000

    deploy: 0e68f31e08457fef34da0ed5c4396436bc43368e

--- search/pages_b.js
+++ search/pages_b.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['welcome_20to_20github_20pages_0',['Welcome to GitHub Pages',['../md_docs_index.html',1,'']]],
-  ['wsl_1',['WSL',['../md_WSL.html',1,'']]]
+  ['wsl_0',['WSL',['../md_WSL.html',1,'']]]
 ];
